abb836152e394a1aef0b78d5b6deeb9932b7fb0b
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-01-09 23:23:50 -0500

    Change processor.

--- process-mmd.sh
+++ process-mmd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+/usr/local/bin/mmd syllabus.mmd
+/usr/local/bin/mmd2tex syllabus.mmd
 sed -i.bak 's/\\slash /\//g' syllabus.tex
 sed -i.bak 's/\\chapter/\\chapter*/' syllabus.tex
 sed -i.bak 's/\\section/\\section*/' syllabus.tex
