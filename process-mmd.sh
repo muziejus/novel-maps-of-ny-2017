112e61e88d24c27d5147ffa0413e006607e746fe
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-08-09 16:15:41 -0400

    Change mmd to md and let syllabus.html be the index.

--- process-mmd.sh
+++ process-mmd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/usr/local/bin/mmd syllabus.mmd
-/usr/local/bin/mmd2tex syllabus.mmd
+/usr/local/bin/mmd syllabus.md
+mv syllabus.html index.html
+/usr/local/bin/mmd2tex syllabus.md
 sed -i.bak 's/\\slash /\//g' syllabus.tex
 sed -i.bak 's/\\chapter/\\chapter*/' syllabus.tex
 sed -i.bak 's/\\section/\\section*/' syllabus.tex
